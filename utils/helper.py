1357f6be26e291abc2f57f39e8fd4acbf8c45cda
dovletov <kurban.dovletov@gmail.com>
2018-08-10 15:53:17 +0200

    Add function to print all shapes

--- utils/helper.py
+++ utils/helper.py
@@ -15,7 +15,7 @@ def saveToHdf5(data, dataset_name, save_path, filename):
     hf.close()
 def loadFromHdf5(load_path, filename, dataset_name):
     """
-    Load data from hdf5 file.
+    Save data into hdf5 file.
     """
     hd = h5py.File(os.path.join(load_path, filename), "r")
     dataset = hd.get(dataset_name)
@@ -23,3 +23,23 @@ def loadFromHdf5(load_path, filename, dataset_name):
     hd.close()
     
     return data
+
+def printShapes():
+    """
+    Prit dataset shapes
+    """
+    for i in range(1,101):
+        filename = 'patient'+str(i).zfill(3)+'_ED.hdf5'
+        x = loadFromHdf5(NP_TRAIN_DIR, filename, 'train')
+        print('File: %s shape: %s' % (filename, str(x.shape)))
+        filename = 'patient'+str(i).zfill(3)+'_ES.hdf5'
+        x = loadFromHdf5(NP_TRAIN_DIR, filename, 'train')
+        print('File: %s shape: %s' % (filename, str(x.shape)))
+
+    for i in range(101,151):
+        filename = 'patient'+str(i).zfill(3)+'_ED.hdf5'
+        x = loadFromHdf5(NP_TEST_DIR, filename, 'test')
+        print('File: %s shape: %s' % (filename, str(x.shape)))
+        filename = 'patient'+str(i).zfill(3)+'_ES.hdf5'
+        x = loadFromHdf5(NP_TEST_DIR, filename, 'test')
+        print('File: %s shape: %s' % (filename, str(x.shape)))
